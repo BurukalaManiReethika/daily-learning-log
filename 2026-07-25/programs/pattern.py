fdc7aa25d0ff09191630955b1407f20892373a0c
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-07-25 06:35:13 +0530

    Add pattern printing script in pattern.py
    This script prints a pattern of substrings from the string 'abcd' in a decreasing and then increasing order.

---
+++ 2026-07-25/programs/pattern.py
@@ -0,0 +1,15 @@
+s="abcd"
+for i in range(len(s),0,-1):
+    print(s[:i])
+for i in range(1,len(s)+1):
+    print(s[:i])
+output:abcd
+abc
+ab
+a
+a
+ab
+abc
+abcd
+
+=== Code Execution Successful ===
