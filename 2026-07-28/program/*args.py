2b8e51b589542fe658d62a4934278dfc5debf328
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-07-28 15:20:32 +0530

    Implement add function with variable arguments

---
+++ 2026-07-28/program/*args.py
@@ -0,0 +1,13 @@
+def add(*args):
+  print(args)
+add(10,20,30)
+(10, 20, 30)
+
+=== Code Execution Successful ===
+or
+def add(*args):
+  print(sum(args))
+add(10,20,30)
+60
+
+=== Code Execution Successful ===
