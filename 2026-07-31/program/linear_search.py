500007305b13abe2ced78bbfe2e2b26d8b795d41
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-07-31 11:31:16 +0530

    Add linear search implementation in linear_search.py
    Implement linear search to find a key in a list.

---
+++ 2026-07-31/program/linear_search.py
@@ -0,0 +1,13 @@
+n=[1,2,3,4,5]
+key=3
+for i in n:
+    if i==key:
+        print("found")
+        break
+    else:
+        print("not found")
+ not found
+not found
+found
+
+=== Code Execution Successful ===
