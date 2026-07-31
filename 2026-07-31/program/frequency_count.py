e19f4eb5630fc870fbc3c8453d692920f4383b44
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-07-31 11:23:50 +0530

    Implement character frequency count in frequency_count.py
    Add frequency count implementation for characters in a string.

---
+++ 2026-07-31/program/frequency_count.py
@@ -0,0 +1,11 @@
+s="mani reethika"
+d={}
+for i in s:
+    if i in d:
+        d[i]+=1
+    else:
+        d[i]=1
+print(d)
+{'m': 1, 'a': 2, 'n': 1, 'i': 2, ' ': 1, 'r': 1, 'e': 2, 't': 1, 'h': 1, 'k': 1}
+
+=== Code Execution Successful ===
