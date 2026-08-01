9096d57bfcadd045c7cc9b61630c10c69b26878d
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-08-01 05:36:42 +0530

    Create constructor.py with student class definition

---
+++ 2026-07-31/program/constructor.py
@@ -0,0 +1,11 @@
+class student:
+  def __init__(self,name,age):
+    self.name=name
+    self.age=age
+s=student("mani reethika",22)
+print(s.name)
+print(s.age)
+mani
+22
+
+=== Code Execution Successful ===
